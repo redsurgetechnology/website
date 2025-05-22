e4d10c70ab1f2f3c7e43ef4e5ff486a0e1e7658a
collinstewart30 <collin.stewart30@yahoo.com>
2025-05-22 10:40:48 -0400

    added first blog

---
+++ js/posts.js
@@ -0,0 +1,11 @@
+function scrollToTargetAdjusted(target) {
+    var element = document.getElementById(target);
+    var headerOffset = 100;
+    var elementPosition = element.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
